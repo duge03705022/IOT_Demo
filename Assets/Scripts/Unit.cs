8eabfe3aa1d77de6c7b9d3143d4e4c0fc81365ef
duge03705022 <duge03705022@gmail.com>
2019-03-28 04:06:28 +0800

    Refine tags flexibility.

--- Assets/Scripts/Unit.cs
+++ Assets/Scripts/Unit.cs
@@ -5,13 +5,16 @@ using UnityEngine;
 public class Unit : MonoBehaviour
 {
     public GameParameter gameParameter;
-    public string RFIDTag;
+    public string[] RFIDTag;
     public int lightValue;
 
     // Start is called before the first frame update
     void Start()
     {
-        gameParameter.unitDic.Add(RFIDTag, gameObject);
+        for (int i = 0; i < RFIDTag.Length; i++)
+        {
+            gameParameter.unitDic.Add(RFIDTag[i], gameObject);
+        }
     }
 
     // Update is called once per frame
